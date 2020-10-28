a4217bbedb837f125b501d3f87e3c37f5dfbeff6
Job <contact@chu.mk>
2020-10-28 21:17:21 +0100

    Removed required tags

--- Data/Models/TlsClientAuth.cs
+++ Data/Models/TlsClientAuth.cs
@@ -6,40 +6,28 @@ namespace Data.Models
     public class TlsClientAuth : EntityBase
     {
 
-        [Required]
         public string CertIssuerDnrfc2253 { get; set; }
 
-        [Required]
         public string CertFingerprintSha1 { get; set; }
 
-        [Required]
         public string CertSubjectDnLegacy { get; set; }
 
-        [Required]
         public string CertPresented { get; set; }
 
-        [Required]
         public string CertIssuerDnLegacy { get; set; }
 
-        [Required]
         public string CertSubjectDn { get; set; }
 
-        [Required]
         public string CertNotBefore { get; set; }
 
-        [Required]
         public string CertNotAfter { get; set; }
 
-        [Required]
         public string CertSubjectDnrfc2253 { get; set; }
 
-        [Required]
         public string CertVerified { get; set; }
 
-        [Required]
         public string CertSerial { get; set; }
 
-        [Required]
         public string CertIssuerDn { get; set; }
     }
 }
--- Data/Models/TlsExportedAuthenticator.cs
+++ Data/Models/TlsExportedAuthenticator.cs
@@ -5,16 +5,12 @@ namespace Data.Models
 {
     public class TlsExportedAuthenticator : EntityBase
     {
-        [Required]
         public string ServerFinished { get; set; }
 
-        [Required]
         public string ClientHandshake { get; set; }
 
-        [Required]
         public string ServerHandshake { get; set; }
 
-        [Required]
         public string ClientFinished { get; set; }
     }
 }
