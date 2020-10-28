de201521c4c19c69fc3fd8a15864a3e44fc4d28f
Job <contact@chu.mk>
2020-10-28 13:28:43 +0100

    Removed all unused imports

--- API/Controllers/HttpRequestsController.cs
+++ API/Controllers/HttpRequestsController.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Data;
-using Data.Models;
 using HttpRequest = Data.Models.HttpRequest;
 
 namespace API.Controllers
--- Data/Models/HttpRequest.cs
+++ Data/Models/HttpRequest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Data.Models.Common;
-using Newtonsoft.Json;
 
 namespace Data.Models
 {
