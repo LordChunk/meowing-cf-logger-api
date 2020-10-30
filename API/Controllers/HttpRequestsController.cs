3cec98f134edaae8201d10e2358f345cd83b5fa4
Job <contact@chu.mk>
2020-10-30 17:51:14 +0100

    Finished converting HttpRequestsController to use repo wrapper

--- API/Controllers/HttpRequestsController.cs
+++ API/Controllers/HttpRequestsController.cs
@@ -4,9 +4,7 @@ using System.Threading.Tasks;
 using API.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Data;
 using Data.Models;
-using Data.Repositories.Common;
 using Data.Repositories.Interfaces;
 using HttpRequest = Data.Models.HttpRequest;
 
@@ -32,7 +30,7 @@ namespace API.Controllers
 
         // GET: api/HttpRequests/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<HttpRequest>> GetHttpRequestAsync(int id)
+        public async Task<ActionResult<HttpRequest>> GetHttpRequest(int id)
         {
             var httpRequest = await _repositoryWrapper.HttpRequest.FindByCondition(r => r.Id == id).FirstOrDefaultAsync();
 
@@ -47,98 +45,98 @@ namespace API.Controllers
         // PUT: api/HttpRequests/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutHttpRequest(int id, HttpRequest httpRequest)
-        //{
-        //    if (id != httpRequest.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHttpRequest(int id, HttpRequest httpRequest)
+        {
+            if (id != httpRequest.Id)
+            {
+                return BadRequest();
+            }
 
-        //    _repositoryWrapper.Entry(httpRequest).State = EntityState.Modified;
+            _repositoryWrapper.HttpRequest.Update(httpRequest);
 
-        //    try
-        //    {
-        //        await _repositoryWrapper.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!HttpRequestExists(id))
-        //        {
-        //            return NotFound();
-        //        }
+            try
+            {
+                await _repositoryWrapper.Save();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HttpRequestExists(id))
+                {
+                    return NotFound();
+                }
 
-        //        throw;
-        //    }
+                throw;
+            }
 
-        //    return NoContent();
-        //}
+            return NoContent();
+        }
 
-        //// POST: api/HttpRequests
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for
-        //// more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        //[HttpPost]
-        //public async Task<ActionResult<HttpRequest>> PostHttpRequest(HttpRequestDto dto)
-        //{
-        //    var httpRequest = ConvertDtoToModel(dto);
-        //    await _repositoryWrapper.HttpRequests.AddAsync(httpRequest);
-        //    await _repositoryWrapper.SaveChangesAsync();
+        // POST: api/HttpRequests
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<HttpRequest>> PostHttpRequest(HttpRequestDto dto)
+        {
+            var httpRequest = ConvertDtoToModel(dto);
+            _repositoryWrapper.HttpRequest.Create(httpRequest);
+            await _repositoryWrapper.Save();
 
-        //    return CreatedAtAction("GetHttpRequest", new { id = httpRequest.Id }, httpRequest);
-        //}
+            return CreatedAtAction("GetHttpRequest", new { id = httpRequest.Id }, httpRequest);
+        }
 
-        //// DELETE: api/HttpRequests/5
-        //[HttpDelete("{id}")]
-        //public async Task<ActionResult<HttpRequest>> DeleteHttpRequest(int id)
-        //{
-        //    var httpRequest = await _repositoryWrapper.HttpRequests.FindAsync(id);
-        //    if (httpRequest == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // DELETE: api/HttpRequests/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<HttpRequest>> DeleteHttpRequest(int id)
+        {
+            var httpRequest = await _repositoryWrapper.HttpRequest.FindByCondition(r => r.Id == id).FirstOrDefaultAsync();
+            if (httpRequest == null)
+            {
+                return NotFound();
+            }
 
-        //    _repositoryWrapper.HttpRequests.Remove(httpRequest);
-        //    await _repositoryWrapper.SaveChangesAsync();
+            _repositoryWrapper.HttpRequest.Delete(httpRequest);
+            await _repositoryWrapper.Save();
 
-        //    return httpRequest;
-        //}
+            return httpRequest;
+        }
 
-        //private bool HttpRequestExists(int id)
-        //{
-        //    return _repositoryWrapper.HttpRequests.Any(e => e.Id == id);
-        //}
+        private bool HttpRequestExists(int id)
+        {
+            return _repositoryWrapper.HttpRequest.FindByCondition(r => r.Id == id).Any();
+        }
 
-        //private HttpRequest ConvertDtoToModel(HttpRequestDto dto)
-        //{
-        //    var model = new HttpRequest()
-        //    {
-        //        Body = dto.Body,
-        //        BodyUsed = dto.BodyUsed,
-        //        Cf = dto.Cf,
-        //        ContentLength = dto.ContentLength,
-        //        Fetchers = dto.Fetchers,
-        //        //Headers = 
-        //        Id = dto.Id,
-        //        Method = dto.Method,
-        //        Redirect = dto.Redirect,
-        //        Url = dto.Url,
-        //    };
+        private HttpRequest ConvertDtoToModel(HttpRequestDto dto)
+        {
+            var model = new HttpRequest()
+            {
+                Body = dto.Body,
+                BodyUsed = dto.BodyUsed,
+                Cf = dto.Cf,
+                ContentLength = dto.ContentLength,
+                Fetchers = dto.Fetchers,
+                //Headers = 
+                Id = dto.Id,
+                Method = dto.Method,
+                Redirect = dto.Redirect,
+                Url = dto.Url,
+            };
 
-        //    var headerList = new List<HttpHeader>();
+            var headerList = new List<HttpHeader>();
 
-        //    dto.Headers.ForEach(header =>
-        //    {
-        //        headerList.Add(new HttpHeader()
-        //        {
-        //            Header = header[0],
-        //            Value = header[1],
-        //            HttpRequestId = model.Id,
-        //        });
-        //    });
+            dto.Headers.ForEach(header =>
+            {
+                headerList.Add(new HttpHeader()
+                {
+                    Header = header[0],
+                    Value = header[1],
+                    HttpRequestId = model.Id,
+                });
+            });
 
-        //    model.Headers = headerList;
+            model.Headers = headerList;
 
-        //    return model;
-        //}
+            return model;
+        }
     }
 }
