6890387438298934cf86f37afb5f1a01fcf9571f
Job <LordChunk@users.noreply.github.com>
2020-11-13 16:12:15 +0100

    Added access to internal methods and added Moq

---
+++ Tests/Repositories/HttpRequestRepositoryUnit.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Data;
+using Data.Models.Common;
+using Data.Repositories.Common;
+using Xunit;
+using Moq;
+
+
+namespace Tests.Repositories
+{
+    public class HttpRequestRepositoryUnit
+    {
+        [Fact]
+        public async void RemoveShouldReturnNullOnFail()
+        {
+            var mockContext = new Mock<ApplicationContext>();
+            var testRepo = new TestRepository(mockContext.Object);
+
+
+            //mockset.Setup(m => m.Remove()).ReturnsAsync()
+        }
+    }
+
+
+    internal class TestEntity : IEntity
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+
+    internal class TestRepository : RepositoryBase<TestEntity>
+    {
+        public TestRepository(ApplicationContext context) : base(context) {}
+    }
+}
