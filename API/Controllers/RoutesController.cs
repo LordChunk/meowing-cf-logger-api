0842919d2fab73e8f5c1de56bd08ee6e33195106
Job <LordChunk@users.noreply.github.com>
2021-06-10 15:24:01 +0200

    Added routes controller

---
+++ API/Controllers/RoutesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace API.Controllers
+{
+    [Route("/")]
+    public class RoutesController : ControllerBase
+    {
+        private readonly IActionDescriptorCollectionProvider _provider;
+
+        public RoutesController(IActionDescriptorCollectionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetRoutes()
+        {
+            var routes = _provider.ActionDescriptors.Items.Select(x => new {
+                Action = x.RouteValues["Action"],
+                Controller = x.RouteValues["Controller"],
+                Name = x.AttributeRouteInfo?.Name,
+                Template = x.AttributeRouteInfo?.Template
+            }).ToList();
+            return Ok(routes);
+        }
+    }
+}
