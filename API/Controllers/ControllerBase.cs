05c46498dbecccbb018c98f70f19af094e603d3c
Job <LordChunk@users.noreply.github.com>
2021-06-14 21:46:51 +0200

    Properly specified consume and produce media types

--- API/Controllers/ControllerBase.cs
+++ API/Controllers/ControllerBase.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("[controller]/[action]")]
     [ApiController]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
     {
     }
