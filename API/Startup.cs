2f6140b8e9c151831248b8f4e1176408b97e962f
Job <LordChunk@users.noreply.github.com>
2021-06-23 18:05:37 +0200

    New migrations are now ran on startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,7 @@
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -39,7 +41,7 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationContext context)
         {
             if (env.IsDevelopment())
             {
@@ -50,6 +52,8 @@ namespace API
                 //app.UseHttpsRedirection();
                 app.UseSentryTracing();
             }
+
+            context.Database.Migrate();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
