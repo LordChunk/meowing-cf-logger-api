76c897ac47943d342ad6cc933e1e52764b89958c
Job <LordChunk@users.noreply.github.com>
2021-06-25 21:20:57 +0200

    Added EF query logging for Sentry and moved DSN into appsettings.json

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Configuration;
+using System.IO;
 using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Sentry;
+using Sentry.AspNetCore;
 
 namespace API
 {
@@ -20,21 +25,30 @@ namespace API
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(Directory.GetCurrentDirectory() + "/../API/appsettings.json")
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseSentry(o =>
                     {
-                        o.Dsn = "https://5b33b0ffed5f4d96be3f0d3635a3f0ac@o272532.ingest.sentry.io/5820135";
+                        o.Dsn = configuration.GetValue<string>("SentryDsn");
+                        
                         // When configuring for the first time, to see what the SDK is doing:
                         //o.Debug = true;
                         // Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
                         // We recommend adjusting this value in production.
                         o.TracesSampleRate = 0.2;
+                        o.AddEntityFramework();
+                        o.Debug = true;
                     });
 
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
